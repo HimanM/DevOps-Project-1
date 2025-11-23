b0b059008f6504e2fd46de208e766d9752909d8b
HimanM <67066047+HimanM@users.noreply.github.com>
2025-11-23 16:23:30 +0530

    feat: Add Next.js configuration with API and metrics rewrites, standalone output, and React Compiler.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,11 +6,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.BACKEND_URL || 'http://evops-project-1-backend:5000/api/:path*',
+        destination: process.env.BACKEND_URL || 'http://devops-project-1-backend:5000/api/:path*',
       },
       {
         source: '/metrics',
-        destination: process.env.BACKEND_URL || 'http://evops-project-1-backend:5000/metrics',
+        destination: process.env.BACKEND_URL || 'http://devops-project-1-backend:5000/metrics',
       },
     ]
   },
